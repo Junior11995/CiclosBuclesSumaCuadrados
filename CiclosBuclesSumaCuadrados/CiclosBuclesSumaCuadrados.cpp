056f8dc031dd295ee9f8a046529b42a6c74f97a2
Junior11995 <junior22ascinsero@gmail.com>
2024-04-18 10:59:04 -0400

    Agregar archivos de proyecto.

---
+++ CiclosBuclesSumaCuadrados/CiclosBuclesSumaCuadrados.cpp
@@ -0,0 +1,26 @@
+/*
+* 
+* 23. Programación en C++ || Ciclos o Bucles || Ejercicio Suma de cuadrados
+
+
+Ejercicio 4: Escriba un programa que tome cada 4 horas la temperatura exterior, 
+leyéndola durante un período de 24 horas. Es decir, debe leer 6 temperaturas.
+Calcule la temperatura media del día, la temperatura más alta y la más baja.
+*/
+
+#include <iostream>
+#include<conio.h>
+
+
+using namespace std;
+
+int main() {
+
+	float hora = 0, minuto =0;
+	int dia,temperatura;
+
+
+
+	_getch();
+	return 0;
+}
